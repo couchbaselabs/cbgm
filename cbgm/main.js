14cbe8e6502b1210b3af9c0b46a9cbf0bc2ff425
Steve Yen <steve.yen@gmail.com>
2013-11-25 22:11:04 -0800

    Render obj JSON.

--- cbgm/main.js
+++ cbgm/main.js
@@ -7,18 +7,30 @@ function deepClone(x) {
   return JSON.parse(JSON.stringify(x));
 }
 
+function render(r, obj) {
+  r.set("obj", obj);
+  r.set("objJSON", JSON.stringify(obj));
+}
+
 function registerEventHandlers(ctx, r) {
   r.on({
     "rebalance": function(event) {
       var obj = r.get("obj");
       if (obj.class != "bucketEvents") {
-        alert("obj is not a bucketEvents");
+        alert("error: obj is not a bucketEvents");
         return;
       }
       var res = rebalance({ prevBucketEvents: deepClone(obj) }) ||
         { err: "unexpected rebalance error" };
-      console.log(res)
-      alert(res.err || "done");
+      console.log(res);
+      if (res.err) {
+        alert("error: " + res.err);
+        return;
+      }
+      alert("done");
+      if (res.nextBucketEvents) {
+        render(r, res.nextBucketEvents);
+      }
     }
   });
   return r;
