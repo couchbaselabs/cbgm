424ea498e8b707f9952327b53aab2b6fa78291d9
Steve Yen <steve.yen@gmail.com>
2013-12-05 23:14:46 -0800

    Some sample nodeCfg's & bucketCfg's.

--- apps/bucketCfg/sectionBuckets.js
+++ apps/bucketCfg/sectionBuckets.js
@@ -1,4 +1,8 @@
 function sectionBuckets(ctx, page) {
+  page.bucketCfgs =
+    _.sortBy(ctx.filterObjs(function(o) { return o.class == "bucketCfg"; }).result,
+             "name");
+
   main(ctx, page, "sectionBuckets");
   sectionBucketsEventHandlers(ctx, page.r);
 }
--- apps/nodeCfg/sectionNodes.js
+++ apps/nodeCfg/sectionNodes.js
@@ -1,4 +1,7 @@
 function sectionNodes(ctx, page) {
+  page.nodeCfgs =
+    _.sortBy(ctx.filterObjs(function(o) { return o.class == "nodeCfg"; }).result,
+             "name");
   main(ctx, page, "sectionNodes");
   sectionNodesEventHandlers(ctx, page.r);
 }
