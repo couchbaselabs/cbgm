5e5a890f3bcdadeebbebe3763685f56afc5f36de
Steve Yen <steve.yen@gmail.com>
2013-12-10 15:50:56 -0800

    Renamed nodeCfg to nodeKnown and nodeWanted.

--- apps/node/sectionNode.js
+++ apps/node/sectionNode.js
@@ -1,26 +1,29 @@
 // Only U/I related JS goes here.
 
 function sectionNode(ctx, page) {
-  page.nodeCfgs = page.nodeCfgs ||
-    _.sortBy(ctx.filterObjs(function(o) { return o.class == "nodeCfg"; }).result,
+  page.nodesKnown = page.nodesKnown ||
+    _.sortBy(ctx.filterObjs(function(o) { return o.class == "nodeKnown"; }).result,
              "name");
-  page.obj = findObj(ctx, page.nodeCfgs, "nodeCfg", page.ident);
+  page.nodesWanted = page.nodesWanted ||
+    _.sortBy(ctx.filterObjs(function(o) { return o.class == "nodeWanted"; }).result,
+             "name");
+  page.obj = findObj(ctx, page.nodesKnown, "nodeKnown", page.ident);
   main(ctx, page, "sectionNode");
   sectionNodeEventHandlers(ctx, page.r);
 }
 
 function sectionNodeEventHandlers(ctx, r) {
   r.on({
-      "newNodeCfg": function(event) {
+      "newNodeKnown": function(event) {
         var names = (event.node.value || "").trim();
         if (!names) {
           return;
         }
         _.each(names.split(","), function(name) {
-            var nodeCfg = ctx.newObj("nodeCfg", { "name": name }).result;
-            var nodeCfgs = r.get("nodeCfgs").unshift(nodeCfg);
-            r.update("nodeCfgs");
-            renderObj(ctx, r, nodeCfg);
+            var nodeKnown = ctx.newObj("nodeKnown", { "name": name }).result;
+            var nodesKnown = r.get("nodesKnown").unshift(nodeKnown);
+            r.update("nodesKnown");
+            renderObj(ctx, r, nodeKnown);
             event.node.value = "";
             event.node.focus();
           });
